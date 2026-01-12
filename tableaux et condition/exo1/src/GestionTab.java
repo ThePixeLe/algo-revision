26239266a7381e0e6cf0c09b0d31932336b219b1
ThePixeLe <lsoilihi@arfp.asso.fr>
2026-01-12 15:27:51 +0100

    test triforce

---
+++ tableaux et condition/exo1/src/GestionTab.java
@@ -0,0 +1,63 @@
+
+import java.util.*;
+
+public class GestionTab {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        System.out.println("Combien de nombre voulez-vous génerer?");
+
+        n = sc.nextInt();
+        int[] _tableau = new int[n];
+        GestionTab.remplirTableau(_tableau);
+        GestionTab.afficherTableau(_tableau);
+        System.out.println("Voici le tableau trié ");
+        GestionTab.trierTableau(_tableau);
+        GestionTab.afficherTableau(_tableau);
+
+        sc.close();
+
+    }
+
+    public static void trierTableau(int[] _tableau) {
+        int encours, pluspetit, temp, j;
+
+        for (encours = 0; encours < _tableau.length - 1; encours++) {
+            pluspetit = encours;
+            for (j = encours + 1; j < _tableau.length; j++) {
+                if (_tableau[j] <= _tableau[pluspetit]) {
+                    pluspetit = j;
+                }
+            }
+            temp = _tableau[encours];
+            _tableau[encours] = _tableau[pluspetit];
+            _tableau[pluspetit] = temp;
+        }
+
+    }
+    // On dit quele tableau est passé en paramêtre par référence
+    public static void remplirTableau(int[] _tableau) {
+        Random aleas = new Random();
+
+        for (int i = 0; i < _tableau.length; i++) {
+
+            _tableau[i] = aleas.nextInt(101);
+
+        }
+
+    }
+
+    public static void afficherTableau(int[] _tableau) {
+        System.out.print("\n[ ");
+        for (int element : _tableau) {
+            System.out.print(element + " ");
+
+        }
+        System.out.print("]\n");
+
+    }
+
+}
+ 
---
+++ tableaux et condition/exo1/src/TriParBulle.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class TriParBulle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        System.out.println("Combien de nombre voulez-vous génerer?");
+
+        n = sc.nextInt();
+        int[] _tableau = new int[n];
+        TriParBulle.remplirTableau(_tableau);
+        TriParBulle.afficherTableau(_tableau);
+        System.out.println("Voici le tableau trié ");
+        TriParBulle.trierTableau(_tableau);
+        TriParBulle.afficherTableau(_tableau);
+
+        sc.close();
+
+    }
+    public static void trierTableau(int[] _tableau) {
+        int i, j, temp;
+        boolean permut;
+
+        for (i = 0; i < _tableau.length - 1; i++) {
+            permut = false;
+            for (j = 0; j < _tableau.length - 1 - i; j++) {
+                if (_tableau[j] > _tableau[j + 1]) {
+                    temp = _tableau[j];
+                    _tableau[j] = _tableau[j + 1];
+                    _tableau[j + 1] = temp;
+                    permut = true;
+                }
+            }
+            if (!permut) {
+                break;
+            }
+        }
+
+    }
+
+
+
+
+
+
+    
+}
