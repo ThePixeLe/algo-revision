e49306ce4ddde266e859aa787ae9054140e7bd7f
ThePixeLe <lsoilihi@arfp.asso.fr>
2026-01-14 09:22:29 +0100

    test again and again

---
+++ tableaux et condition/exo1/src/test.java
@@ -0,0 +1,30 @@
+/*import java.util.*;
+
+
+public class test {
+
+
+
+    public static void direBonjour(String nom) {
+    
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bonjour " + nom + "!");
+
+        sc.close();
+    }
+}*/
+
+
+import java.util.*;
+
+public class test {
+
+public static void direBonjour(String nom) {
+System.out.println("Bonjour " + nom + "!");
+}
+
+public static void main(String[] args) {
+direBonjour("H1m0t3p3");
+}
+}
